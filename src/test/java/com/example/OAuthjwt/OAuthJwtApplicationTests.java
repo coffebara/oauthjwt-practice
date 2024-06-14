3fc0c655fc1df61a07edeec75505340cfab40fe8
allready-dawn <qwshpke@naver.com>
2024-06-14 14:13:01 +0900

    first commit

---
+++ src/test/java/com/example/OAuthjwt/OAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OAuthjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
